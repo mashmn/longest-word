c809b0fb86dd71208c1c88d66296fa50f2b521c1
mashmn <borne.god@gmail.com>
2018-07-01 23:09:58 -0400

    Using ArrayList instead

--- src/main/java/com/boot/longest_word/MyApp.java
+++ src/main/java/com/boot/longest_word/MyApp.java
@@ -1,46 +1,54 @@
 package com.boot.longest_word;
 
+import java.util.ArrayList;
 /**
  * Application which finds the longest words and its length!
  * Author: Mahesh Nair
  * GitHub: https://github.com/mashmn/
  */
-import java.util.Scanner;
 
 public class MyApp {
 	
-	public static void main (String args[])  {
+	public static void main (String args[]) {
+		new MyApp().LongestWord();
+		System.exit(0);	
 
-		System.out.println("Type a sentence:");	
-		ScanSentence();
-		System.exit(0);
 	}
 	
-	public static void ScanSentence() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println();		
-		System.out.println(LongestWord(scan.nextLine()));
-		scan.close();		
+	// ArrayList with variety of sentences
+	public static ArrayList<String> mySentences() {
+		ArrayList<String> mySentence = new ArrayList<String>();
+		mySentence.add("Hi");
+		mySentence.add("1234 is not a word!");
+		mySentence.add("this words is a mistake jelly123bean.");
+		return mySentence;
 	}
 	
-	public static String LongestWord(String str) {
+	/** Method that splits sentences to smaller words 
+	 *	finds the longest word and prints it out. 
+	 */	 
+	public void LongestWord() {
 		
-// 		Added regular expression to consider legible words consisting of alphabets only.
-//		String[] words = str.split("\\s*[^a-zA-Z0-9]+\\s*");
-		String[] words = str.split("\\s*[^a-zA-Z]+\\s*");
+		ArrayList<String> str = mySentences();; 
 		
-		int longestStringLength = 0;
-		String longestWord = "";
-
-		for (String word: words) {
+		for (String sentence: str) {
+			String[] words = sentence.split("\\s*[^a-zA-Z]+\\s*");
 			
-			if (longestStringLength < word.length()) {
-				longestStringLength = word.length();
-				longestWord = word;
+			int longestStringLength = 0;
+			String longestWord = "";
+
+			for (String word: words) {
+				
+				if (longestStringLength < word.length()) {
+					longestStringLength = word.length();
+					longestWord = word;
+				}
+				
 			}
+			String printOut = "In the sentence \"" + sentence +"\" -----> Longest word: \"" + longestWord + "\" and length: " + longestStringLength + ".";
+			System.out.println(printOut);			
 		}
-		
-		return ("The longest word is \"" + longestWord + "\" and the length is " + longestStringLength + ".");
 	}
+	
 	
 }
