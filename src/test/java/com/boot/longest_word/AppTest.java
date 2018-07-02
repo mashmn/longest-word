f076b6ffb1a90597e1c458c540850cfdb6a5c138
mashmn <borne.god@gmail.com>
2018-07-01 22:54:02 -0400

    Maven project initiation

---
+++ src/test/java/com/boot/longest_word/AppTest.java
@@ -0,0 +1,23 @@
+package com.boot.longest_word;
+
+import junit.framework.*;
+
+/**
+ * Unit test to check if junit is setup
+ */
+public class AppTest extends TestCase
+{
+    protected int value1, value2;
+    
+    // assigning the values
+    protected void setUp(){
+       value1 = 3;
+       value2 = 3;
+    }
+
+    // test method to add two values
+    public void testAdd(){
+       double result = value1 + value2;
+       assertTrue(result == 6);
+    }
+}
